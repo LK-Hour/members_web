d902cf40e363527b7fc7d2fd3945e171fa99ca42
LK-Hour <l.kimhour.609@gmail.com>
2025-02-10 12:22:31 +0700

    update the query

--- api_server.js
+++ api_server.js
@@ -150,7 +150,7 @@ app.post("/membersadd", async (req, res) => {
     } = req.body;
 
     // Input validation
-    if (!id_mem || !name_mem || !email_mem || !password_mem) {
+    if (!name_mem || !email_mem || !password_mem) {
       return res.status(400).json({ error: "Required fields are missing" });
     }
 
